a37b963e0fea227464106ccd051fbe9f3ae13dba
Doyeon04 <rlaehdus0411@naver.com>
2024-03-29 15:27:59 +0900

    Init: project 환경 설정

---
+++ src/main/java/contest/collectingbox/CollectingBoxApplication.java
@@ -0,0 +1,13 @@
+package contest.collectingbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectingBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectingBoxApplication.class, args);
+    }
+
+}
---
+++ src/main/java/contest/collectingbox/entity/TestEntity.java
@@ -0,0 +1,15 @@
+package contest.collectingbox.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class TestEntity{
+    @Id @GeneratedValue
+    private Long id;
+
+}
