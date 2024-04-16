3f4b3850aaec4d3c86cfbc6f4e27d8d12e9b63df
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-04-17 00:48:04 +0900

    chore: Swagger 설정 추가

--- src/main/java/contest/collectingbox/module/autocomplete/dto/AutoCompleteResponseDto.java
+++ src/main/java/contest/collectingbox/module/autocomplete/dto/AutoCompleteResponseDto.java
@@ -1,14 +1,18 @@
 package contest.collectingbox.module.autocomplete.dto;
 
-import java.util.ArrayList;
-import java.util.List;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Getter
 @NoArgsConstructor
 public class AutoCompleteResponseDto {
+
+    @Schema(description = "검색어 자동완성 목록", example = "[\"강남구 대치동\", \"강남구 삼성동\", \"강남구 청담동\"]")
     private List<String> items = new ArrayList<>();
 
     public AutoCompleteResponseDto(List<String> items) {
--- src/main/java/contest/collectingbox/module/collectingbox/dto/CollectingBoxResponse.java
+++ src/main/java/contest/collectingbox/module/collectingbox/dto/CollectingBoxResponse.java
@@ -1,6 +1,7 @@
 package contest.collectingbox.module.collectingbox.dto;
 
 import contest.collectingbox.module.collectingbox.domain.CollectingBox;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,17 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CollectingBoxResponse {
 
+    @Schema(description = "수거함 ID", example = "2571")
     private Long id;
+
+    @Schema(description = "위도", example = "37.4888178446615")
     private Double latitude;
+
+    @Schema(description = "경도", example = " 126.902998281977")
     private Double longitude;
+
+    @Schema(description = "태그", example = "CLOTHES",
+            allowableValues = {"CLOTHES", "LAMP", "BATTERY", "MEDICINE", "TRASH"})
     private String tag;
 
     public static CollectingBoxResponse fromEntity(CollectingBox collectingBox) {
