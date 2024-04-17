91abdeafc4ee78979972f371f911d67c7efb0d20
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-04-18 00:14:22 +0900

    refactor: 응답 구조 변경

--- src/main/java/contest/collectingbox/global/common/ApiResponse.java
+++ src/main/java/contest/collectingbox/global/common/ApiResponse.java
@@ -1,31 +1,29 @@
 package contest.collectingbox.global.common;
 
-import static org.springframework.http.HttpStatus.*;
-
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.OK;
+
 @Getter
 public class ApiResponse<T> {
+
+    private static final String SUCCESS = "SUCCESS";
 
     private int code;
     private HttpStatus status;
     private String message;
     private T data;
 
-    public ApiResponse(HttpStatus status, String message, T data) {
+    private ApiResponse(HttpStatus status, String message, T data) {
         this.code = status.value();
         this.status = status;
         this.message = message;
         this.data = data;
     }
 
-    public static <T> ApiResponse<T> of(HttpStatus status, String message, T data) {
-        return new ApiResponse<>(status, message, data);
-    }
-
     public static <T> ApiResponse<T> ok(T data) {
-        return of(OK, "SUCCESS", data);
+        return new ApiResponse<>(OK, SUCCESS, data);
     }
 
 }
