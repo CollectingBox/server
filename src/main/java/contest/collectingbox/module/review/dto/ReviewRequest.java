a1bb1bb6802a10f123b6908b83aa50452026052f
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-04-17 20:25:43 +0900

    Review API Swagger 설정 추가 (#26)
    * chore: Review 관련 스웨거 설정 추가
    
    * chore: swagger 리뷰 내용 allowableValues 추가

--- src/main/java/contest/collectingbox/module/review/dto/ReviewRequest.java
+++ src/main/java/contest/collectingbox/module/review/dto/ReviewRequest.java
@@ -3,6 +3,7 @@ package contest.collectingbox.module.review.dto;
 import contest.collectingbox.module.collectingbox.domain.CollectingBox;
 import contest.collectingbox.module.review.domain.Review;
 import contest.collectingbox.module.review.domain.Tag;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ReviewRequest {
+    @Schema(description = "리뷰 내용", example = "EXIST", allowableValues = {"EXIST", "DISAPPEAR"})
     private String content;
 
     public Review toEntity(CollectingBox collectingBox) {
