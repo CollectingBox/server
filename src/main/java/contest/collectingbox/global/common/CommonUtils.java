8b391fd3ee86453a78087729cce392cfbdb6ab60
Doyeon04 <rlaehdus0411@naver.com>
2024-04-19 23:06:10 +0900

    feat: 날짜 변환 시 DateTimeParseException 예외 처리

--- src/main/java/contest/collectingbox/global/common/CommonUtils.java
+++ src/main/java/contest/collectingbox/global/common/CommonUtils.java
@@ -1,12 +1,23 @@
 package contest.collectingbox.global.common;
 
+import static contest.collectingbox.global.exception.ErrorCode.NOT_FOUND_COLLECTING_BOX;
+
+import contest.collectingbox.global.exception.CollectingBoxException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CommonUtils {
     public static String formatDate(String inputDate) {
-        LocalDateTime dateTime = LocalDateTime.parse(inputDate,
-                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSS"));
-        return dateTime.format(DateTimeFormatter.ofPattern("yy.MM.dd"));
+        try{
+            LocalDateTime dateTime = LocalDateTime.parse(inputDate,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));
+            return dateTime.format(DateTimeFormatter.ofPattern("yy.MM.dd"));
+        }catch(DateTimeParseException e){
+            log.error("exception message = {}", e.getMessage());
+            return null;
+        }
     }
 }
