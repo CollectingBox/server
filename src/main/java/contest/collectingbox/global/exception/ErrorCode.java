ae53f6e3e6605f6cd1996d804f49c2fde70b8c69
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-04-21 20:12:41 +0900

    수거함 상세보기 조회 기능 예외처리 (#43)
    * feat: 수거함 상세정보 조회 시 존재하지 않는 수거함일 경우 예외 처리
    
    * refactor: 공통 메서드 모음 클래스 생성
    
    * feat: 날짜 변환 시 DateTimeParseException 예외 처리
    
    * feat: IllegalArgumentException 처리 핸들러 추가
    
    * test: 수거함 상세정보 조회 테스트 성공하도록 수정
    
    * refactor: 날짜 유틸리티 클래스 이름 변경 및 @UtilityClass로 생성자 private으로 변경
    
    * refactor: IllegalArgument 예외 처리 삭제
    
    * style: ErrorCode 세미클론 제거 후 쉼표로 변경

--- src/main/java/contest/collectingbox/global/exception/ErrorCode.java
+++ src/main/java/contest/collectingbox/global/exception/ErrorCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 @AllArgsConstructor
@@ -12,10 +13,13 @@ public enum ErrorCode {
     // 400
     NOT_SELECTED_TAG(BAD_REQUEST, "수거함 태그는 반드시 한 개 이상 설정해야 합니다."),
     MISSING_REQUEST_PARAM(BAD_REQUEST, "필수 요청 파라미터가 존재하지 않습니다."),
-    MISMATCH_REQUEST_PARAM(BAD_REQUEST, "요청 파라미터가 유효하지 않습니다.");
+    MISMATCH_REQUEST_PARAM(BAD_REQUEST, "요청 파라미터가 유효하지 않습니다."),
 
     // 404
+    NOT_FOUND_COLLECTING_BOX(NOT_FOUND, "해당 수거함이 존재하지 않습니다."),
 
+    // 임시
+    NOT_FOUND_TAG(NOT_FOUND, "해당 이름과 일치하는 수거함 태그가 없습니다.");
     // 409
 
     // 500
--- src/main/java/contest/collectingbox/global/exception/GlobalExceptionHandler.java
+++ src/main/java/contest/collectingbox/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package contest.collectingbox.global.exception;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
