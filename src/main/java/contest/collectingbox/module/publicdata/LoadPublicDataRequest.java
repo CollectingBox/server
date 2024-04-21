2ea30d9932d940f56e26e8c5716d3a494d85d02b
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-04-21 16:55:21 +0900

    feat: 공공데이터 api 호출 및 주소 검색 키워드 추출 로직 구현

---
+++ src/main/java/contest/collectingbox/module/publicdata/LoadPublicDataRequest.java
@@ -0,0 +1,16 @@
+package contest.collectingbox.module.publicdata;
+
+import contest.collectingbox.module.collectingbox.domain.Tag;
+import lombok.Data;
+
+@Data
+public class LoadPublicDataRequest {
+
+    private String sigungu;
+    private Tag tag;
+    private String callAddress;
+
+    public String getUrlWithPerPage(String apiKey, int perPage) {
+        return String.format("https://api.odcloud.kr/api%s?serviceKey=%s&perPage=%d", callAddress, apiKey, perPage);
+    }
+}
