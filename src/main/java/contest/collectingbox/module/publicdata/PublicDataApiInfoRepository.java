e5a5d592b98fa9e71225797f4d5d62ab2e091e13
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-04-24 00:00:05 +0900

    feat: 공공데이터 API 정보 저장 기능 구현

---
+++ src/main/java/contest/collectingbox/module/publicdata/PublicDataApiInfoRepository.java
@@ -0,0 +1,6 @@
+package contest.collectingbox.module.publicdata;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublicDataApiInfoRepository extends JpaRepository<PublicDataApiInfo, Long> {
+}
