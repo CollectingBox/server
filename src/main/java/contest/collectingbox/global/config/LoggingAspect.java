d4ffb88aff4d8de825f42c66c83199b824dfcd0f
Doyeon04 <rlaehdus0411@naver.com>
2024-05-05 15:48:10 +0900

    chore: 컨트롤러 로깅용 Aspect 생성

---
+++ src/main/java/contest/collectingbox/global/config/LoggingAspect.java
@@ -0,0 +1,36 @@
+package contest.collectingbox.global.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+    @Pointcut("within(contest.collectingbox.module..*Controller)")
+    private void cut() {
+    }
+
+    @Around("cut()")
+    public Object doLogRequest(final ProceedingJoinPoint joinPoint) throws Throwable {
+        final HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest();
+        final String ipAddr = request.getRemoteAddr();
+        final String method = request.getMethod();
+        final String requestURI = request.getRequestURI();
+        final Object[] args = joinPoint.getArgs();
+
+        log.info("[REQUEST] {} {} {} args={}", ipAddr, method, requestURI, args);
+        Object result = joinPoint.proceed();
+        log.info("[RESPONSE] {}", result);
+        return result;
+
+    }
+}
--- src/main/java/contest/collectingbox/module/publicdata/LoadCsvPublicDataRequest.java
+++ src/main/java/contest/collectingbox/module/publicdata/LoadCsvPublicDataRequest.java
@@ -2,8 +2,10 @@ package contest.collectingbox.module.publicdata;
 
 import contest.collectingbox.module.collectingbox.domain.Tag;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class LoadCsvPublicDataRequest {
 
     private String sigungu;
