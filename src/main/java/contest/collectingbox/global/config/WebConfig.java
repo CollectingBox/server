5e47f03c03ab650cd683d377483f2c63dfeed8e2
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-06-13 15:25:12 +0900

    config: ArgumentResolver 설정 추가

--- src/main/java/contest/collectingbox/global/config/WebConfig.java
+++ src/main/java/contest/collectingbox/global/config/WebConfig.java
@@ -1,16 +1,28 @@
 package contest.collectingbox.global.config;
 
+import contest.collectingbox.global.config.web.PointArgumentResolver;
+import contest.collectingbox.global.config.web.TagsArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .maxAge(3000);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new PointArgumentResolver());
+        resolvers.add(new TagsArgumentResolver());
     }
 }
