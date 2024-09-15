7d593d38abab29906d7ac3a505c5838515a58b74
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-09-16 02:37:02 +0900

    fix: JSON 매핑이 안되어 공공데이터 저장 안되는 문제 해결

--- src/main/java/contest/collectingbox/module/publicdata/AddressInfoMapper.java
+++ src/main/java/contest/collectingbox/module/publicdata/AddressInfoMapper.java
@@ -5,27 +5,39 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.json.JSONObject;
 
+import static contest.collectingbox.module.publicdata.AddressInfoDto.*;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AddressInfoMapper {
     public static AddressInfoDto jsonObjectToAddressInfoDto(JSONObject document, Tag tag) {
         JSONObject address = document.getJSONObject("address");
         JSONObject roadAddress = document.getJSONObject("road_address");
-        return AddressInfoDto.builder()
+
+        AddressInfoDtoBuilder builder = builder()
                 .longitude(document.getDouble("x"))
                 .latitude(document.getDouble("y"))
+                .tag(tag);
+
+        return buildWithRoadAddress(buildWithAddress(builder, address), roadAddress, tag).build();
+    }
+
+    private static AddressInfoDtoBuilder buildWithAddress(AddressInfoDtoBuilder builder, JSONObject address) {
+        if (address.isEmpty()) {
+            return builder;
+        }
+        return builder
                 .sido(address.getString("region_1depth_name"))
                 .sigungu(address.getString("region_2depth_name"))
                 .dong(address.getString("region_3depth_h_name"))
-                .roadName(roadAddress.getString("address_name"))
-                .streetNum(address.getString("address_name"))
-                .name(getDetailName(tag, roadAddress.getString("building_name")))
-                .tag(tag)
-                .build();
+                .streetNum(address.getString("address_name"));
     }
 
-    private static String getDetailName(Tag tag, String buildingName) {
-        return buildingName.isEmpty() ?
-                (tag.name().equals("TRASH") ? tag.getLabel() : tag.getLabel() + " 수거함")
-                : buildingName + " 근처 수거함";
+    private static AddressInfoDtoBuilder buildWithRoadAddress(AddressInfoDtoBuilder builder, JSONObject roadAddress, Tag tag) {
+        if (roadAddress.isEmpty()) {
+            return builder.name(tag.name().equals("TRASH") ? tag.getLabel() : tag.getLabel() + " 수거함");
+        }
+        return builder
+                .roadName(roadAddress.getString("address_name"))
+                .name(roadAddress.getString("building_name") + " 근처 수거함");
     }
 }
