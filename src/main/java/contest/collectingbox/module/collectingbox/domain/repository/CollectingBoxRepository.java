ced5a524bb3ccf95770bdea6e668eba2bed481e3
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-09-19 17:20:57 +0900

    style: 사용되지 않는 코드 제거

--- src/main/java/contest/collectingbox/module/collectingbox/domain/repository/CollectingBoxRepository.java
+++ src/main/java/contest/collectingbox/module/collectingbox/domain/repository/CollectingBoxRepository.java
@@ -1,17 +1,7 @@
 package contest.collectingbox.module.collectingbox.domain.repository;
 
 import contest.collectingbox.module.collectingbox.domain.CollectingBox;
-import contest.collectingbox.module.collectingbox.domain.Tag;
-import contest.collectingbox.module.location.domain.DongInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface CollectingBoxRepository extends JpaRepository<CollectingBox, Long>, CollectingBoxRepositoryCustom {
-
-    @Query("select c from CollectingBox c join c.location l where l.dongInfo = :dongInfo and c.tag in :tags")
-    List<CollectingBox> findAllByDongInfoAndTags(@Param("dongInfo") DongInfo dongInfo,
-                                                 @Param("tags") List<Tag> tags);
 }
